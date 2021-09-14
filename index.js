e3462f58eacaf85e74be7cb7c9ec869db61eca54
Kadu Cury <carlosercury@gmail.com>
2021-09-13 22:18:16 -0300

    Alterando o código do Cadastro de Peças

--- index.js
+++ index.js
@@ -1,31 +1,35 @@
-let listaPecas=[]
+let listaPecas=[ ]
 let nomePeca='Peça Teste'
 let pesoPeca=300
 let podeCadastrar=true
+const fg_red = '\x1b[31m'
+const fg_green = '\x1b[32m'
+const fg_yellow = '\x1b[33m'
+const reset_color = '\x1b[0m'
 
 if (pesoPeca<100){
-    console.log('Não há possibilidade de cadastro pois a peça não atinge o peso mínimo!')
+    console.log(fg_red, 'Não há possibilidade de cadastro pois a peça não atinge o peso mínimo!', reset_color)
     podeCadastrar=false
 }
 
 if (listaPecas.length>10){
-    console.log('Limite de peças cadastradas atingido!')
+    console.log(fg_red, 'Limite de peças cadastradas atingido!', reset_color)
     podeCadastrar=false
 }
 
 if (nomePeca.length<3){
-    console.log('Para cadastrar a peça seu nome tem de ter mais de três letras!')
+    console.log(fg_red, 'Para cadastrar a peça seu nome tem de ter mais de três letras!', reset_color)
     podeCadastrar=false
 }
 
 if (podeCadastrar){
     listaPecas.push(nomePeca)
-    console.log('Peça cadastrada com sucesso!')
+    console.log(fg_green, 'Peça cadastrada com sucesso!', reset_color)
 }   else{
-    console.log('Houve um problema ao cadastrar a peça!')
+    console.log(fg_red,'Houve um problema ao cadastrar a peça!', reset_color)
     }
 
-console.log('Listando as peças cadastradas.')
+console.log(fg_yellow, 'Listando as peças cadastradas.', reset_color)
 for (let i=0;i<listaPecas.length;i++){
-    console.log(listaPecas[i])
+    console.log(fg_yellow, listaPecas[i], reset_color)
 }
