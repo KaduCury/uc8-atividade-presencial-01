0b5a058221f9cb7290a48138dece07f8f059afdf
KaduCury <carlosercury@gmail.com>
2021-09-15 18:45:04 -0300

    Listas estão saindo de maneira diferente.

--- Cadastro-de-eventos.js
+++ Cadastro-de-eventos.js
@@ -1,18 +1,21 @@
-let ListaDeParticipantes= [ ]
-let DataDoEvento= Date('2021-10-30')
-let DataAtual= Date('2021-09-14')
-let DataDeNascimento= Date('1987-03-15')
+let ListaDeParticipantes= [ 'Joe', 'Johnny', 'Junior']
+let ListaDePalestrantes= [ 'Prof. X', 'Mr. Y', 'Doc. Z']
+let DataDoEvento= Date(2021-08-30)
+let DataAtual= Date(2021-09-14)
+let Idade= 18
 let Nome= 'Jonas Jr'
 let PodeCadastrar= true
 let Dados= 'RG, CPF e Endereço'
-let TodosDados= [Nome, DataDeNascimento, Dados]
+let TodosDados= [Nome, Idade, Dados]
+/*   let DataDeNascimento= Date('2003-09-01')   */
+
 
 if (DataDoEvento<DataAtual){
     console.log('Data inválida! A data informada é anterior a data atual.')
     PodeCadastrar= false
 }
 
-if (DataDeNascimento){ /*DataAtual-DataDeNascimento>=18 anos*/ 
+if (Idade<18){                                         /*DataAtual-DataDeNascimento>=18 anos*/ 
     console.log('Evento exclusivo para maiores de 18 anos')
     PodeCadastrar= false
 }
@@ -22,10 +25,10 @@ if (ListaDeParticipantes.length>100){
     PodeCadastrar= false
 }
 
-if (TodosDados=false){ /*Validar TodosDados*/
+/* if (TodosDados=false){                                                 Validar TodosDados
     console.log('Os dados informados são inválidos!')
-    PodeCadastrar= false
-}
+    PodeCadastrar= false         
+}                                                                                         */
 
 if (PodeCadastrar){
     ListaDeParticipantes.push(Nome)
@@ -37,4 +40,4 @@ if (PodeCadastrar){
 console.log('Listando Participantes: ')
 for (let i=0;i<ListaDeParticipantes.length;i++){
     console.log(ListaDeParticipantes[i])
-}
+}console.log('Lista de Palestrantes: ', ListaDePalestrantes)
