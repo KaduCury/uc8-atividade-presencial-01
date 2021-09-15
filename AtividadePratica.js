763d02f739c7cac1352a1ddc85c4533251c564f4
KaduCury <carlosercury@gmail.com>
2021-09-15 18:23:28 -0300

    Arrumando a concordância do texto.

--- AtividadePratica.js
+++ AtividadePratica.js
@@ -1,6 +1,8 @@
 for (let i=0;i<=9;i++){
     if (i==0){
         console.log('Não há alunos presentes!', i)
+    }   else if (i==1){
+        console.log ('O número de alunos presentes é ÍMPAR! Só tem', i,'aluno presente.')
     }   else if (i%2==0){
         console.log('O número de alunos presentes é PAR! Os alunos presentes são', i)
     }   else {
